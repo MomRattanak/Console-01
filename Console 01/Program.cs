f400165cbccc76690b2c2edd323884e80560acdb
mom rattanak <rattanak36@outlook.com>
2022-08-27 11:28:08 +0700

    last

--- Console 01/Program.cs
+++ Console 01/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Console_01
 {
     class Program
     {
-        static void Main(string[] args)
+        public class myclass
         {
+            static float proE = 20.1f;
+            public static void Main(string[] args)
+            {
+                bool a = true;
+                bool b = false;
+
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.ReadLine();
+            }
+
         }
+
+        
     }
 }
