dd6d75cf3214879d8f4644bd2e8e96045180ecd1
mom rattanak <rattanak36@outlook.com>
2022-08-27 16:31:37 +0700

    last

--- Console 01/Program.cs
+++ Console 01/Program.cs
@@ -13,6 +13,12 @@ namespace Console_01
                 int b = 5;
 
                 Console.WriteLine(a + b);
+                Console.WriteLine(div(10, 30));
+                Console.WriteLine(Add(30, 20));
+                int result = Add(10, 20);
+                Console.WriteLine(result);
+                suu();
+                sum();
                 writeline();
                 Console.ReadLine();
             }
@@ -21,7 +27,24 @@ namespace Console_01
                 Console.WriteLine("I am called from a method");
                 Console.Read();
             }
+            public static void sum()
+            {
+                Console.WriteLine("Sum of 4 and 7:"+(4 + 7));
+            }
+            public static void suu(string names = "Srey Nich")
+            {
+                Console.WriteLine(names);
+                
+            }
+            public static int Add(int num1, int num2)
+            {
 
+                return num1 + num2;
+            }
+            public static float div(float num1, float num2)
+            {
+                return num1/ num2;
+            }
         }
 
         
